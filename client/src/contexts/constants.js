27bde874697b545e23a23587aeb8e20a1e0f26f5
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-05-16 08:32:32 +0700

    fix bug CORS

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,7 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'https://protected-earth-98280-4f8eea74f6f5.herokuapp.com/api'
+		// ? 'http://localhost:5000/api'
 		: 'some'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const app = express();
 app.use(express.json())
 
 const corsOptions ={
-    origin:'http://localhost:5000', 
+    origin:'http://localhost:3000', 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
