ae225b647489135c82d9e460ac4bd15132c51cc1
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-05-16 12:32:21 +0700

    refresh code

--- server/index.js
+++ server/index.js
@@ -4,13 +4,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 
-const corsOptions ={
-    origin: ['https://protected-earth-98280-4f8eea74f6f5.herokuapp.com'], 
-    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin: ['https://protected-earth-98280-4f8eea74f6f5.herokuapp.com'], 
+//     methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
+
 
 // Import from created file
 require('dotenv').config()
@@ -38,6 +39,7 @@ connectDB();
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/auth', authRouter);
 app.use('/api/posts', postRouter);
